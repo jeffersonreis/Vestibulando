6d68fa25cb63578eabbdede58be652506e9eb15e
Jefferson <jeffersoon.bnu@hotmail.com>
2018-10-11 22:27:27 -0300

    Primeira Vesão
    Primeira versão de testes do git

---
+++ login.php
@@ -0,0 +1,44 @@
+<?php 
+  
+  // Dados do MySQL
+  $host = 'localhost';
+  $usuario = 'root';
+  $pass = '';
+  $db = 'vestibulando';
+
+  // Conectando no mysql
+  $connect = mysqli_connect($host, $usuario, $pass, $db) or die (mysql_error());
+  mysqli_select_db($connect, $db); // Tipo 'use'
+  mysqli_autocommit($connect, TRUE);
+
+
+  // Pegando as informacoes do login
+  $login = $_POST['login'];
+  $entrar = $_POST['entrar'];
+  $senha = $_POST['senha'];
+
+  $comando_verificar = "SELECT * FROM usuarios WHERE login = '" . $login . "' AND senha = '" . $senha . "'";
+  echo $comando_verificar;
+
+    // Se foi pressionado o botao ENTRAR...
+    if (isset($entrar)) {
+
+      //Verifica se existe o usuario com a mesma senha...
+      $verifica = mysqli_query($connect, $comando_verificar);
+
+        // Se retornar nenhum, não tem.
+        if (mysqli_num_rows($verifica)<=0){
+          echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.html';</script>";
+          #echo 'LOGIN ERRADO';
+          die(); 
+
+        // Se tiver, volta para o HOME.
+        }else{
+          #echo 'LOGIN CERTO';
+          setcookie("login",$login);
+          header("Location:index.php");
+        }
+    }
+
+
+?>
