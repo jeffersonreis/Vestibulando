ffe6ba3c571e171ad4f96b67585bf775d5e89512
jeffluis <33864468+jeffluis@users.noreply.github.com>
2018-11-19 21:30:14 -0200

    CADASTRO E LOGIN 100% FUNCIONAL
    Foi tirado todos os bugs e agora está 100% ok.

--- cadastrando.php
+++ cadastrando.php
@@ -1,6 +1,6 @@
-
 <?php
-    // Essa página tem a intenção de fazer a conexão com o BD e inserir as informações.
+  
+  // Essa página tem a intenção de fazer a conexão com o BD e inserir as informações.
 	
   // INFORMAÇÕES DO MYSQL;
 	$host = "localhost";
@@ -24,28 +24,49 @@
 	$dat_nasc = $_POST['data'];
 	$email = $_POST['email'];
 	$senha = $_POST['senha'];  
+  $conf_senha = $_POST['conf_senha'];
 
 	// TESTE PARA VER SE TA TUDO OK COM AS VARIAVEIS;
-    echo "\nlogin eh: ", $nome;
-    echo "\nsobrenome eh: ", $sobrenome;
-    echo "\ndat_nasc: ", $dat_nasc;
-    echo "\nemail: ", $email;
-    echo "\nsenha eh: ", $senha;
+  echo "\nlogin eh: ", $nome;
+  echo "\nsobrenome eh: ", $sobrenome;
+  echo "\ndat_nasc: ", $dat_nasc;
+  echo "\nemail: ", $email;
+  echo "\nsenha eh: ", $senha;
+  echo "\nconfsenha eh: ", $conf_senha;
 
+  // Verificar se não tem nenhum usuario com o mesmo email
+  $comando_verificar = "SELECT * FROM usuarios WHERE email = '" . $email . "'";
+  $verifica = mysqli_query($conexao, $comando_verificar);
+  
+  // Se retornar nenhum, não tem.
 
-    // COMANDO MODELO PARA OS DEMAIS (ALDELIR);
-    #$comando = "INSERT INTO usuarios(login, senha) VALUES ('" . $nome . "','" . $senha . "')";
+  if ($conf_senha != $senha){
+    echo"<script language='javascript' type='text/javascript'>alert('Senhas não correspondem');window.location.href='login.html';</script>";
+       #echo 'email ERRADO';
+    die(); 
 
-    // COMANDO PARA ADICIONAR NO BD;
-    $comando = "INSERT INTO usuarios(nome, sobrenome, dat_nasc, email, senha) VALUES ('" . $nome . "','" . $sobrenome . "','" . $dat_nasc . "','" . $email . "','" . $senha . "')";
+  }
 
-    // MOSTRA O COMANDO PARA VER SE TÁ OK;
-    echo "comando eh: ", $comando;
+  if (mysqli_num_rows($verifica)>0){
+        echo"<script language='javascript' type='text/javascript'>alert('Email já usado');window.location.href='login.html';</script>";
+       #echo 'email ERRADO';
+        die(); 
+  }
+
+  // COMANDO MODELO PARA OS DEMAIS (ALDELIR);
+  #$comando = "INSERT INTO usuarios(login, senha) VALUES ('" . $nome . "','" . $senha . "')";
+
+  // COMANDO PARA ADICIONAR NO BD;
+  $comando = "INSERT INTO usuarios(nome, sobrenome, dat_nasc, email, senha) VALUES ('" . $nome . "','" . $sobrenome . "','" . $dat_nasc . "','" . $email . "','" . $senha . "')";
+
+  // MOSTRA O COMANDO PARA VER SE TÁ OK;
+  echo "comando eh: ", $comando;
 
 
 	#mysqli_query($conexao, $comando);
 	mysqli_query($conexao, $comando);
 
 	mysqli_close($conexao);
+
 
 ?>
